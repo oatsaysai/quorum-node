c3a9c440f0285190adec9f1ef03d5b19545d74f9
Sakkarin Saysai <sakkarin@blockfint.com>
2018-07-25 12:54:22 +0700

    Add getResult.sh

---
+++ getResult.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -u
+set -e
+
+rm -rf result/*
+mkdir result
+containers=$(docker ps | grep "quorum-api:latest" | awk '{ print $1 }')
+var=1
+for container in $containers
+do
+  docker cp $container:/blockchain/result.csv $PWD/result/"result$var.csv"
+  ((var++))
+done
